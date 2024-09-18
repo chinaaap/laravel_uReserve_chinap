76574f08abb3c1efb675db89c2881f1aef3659e9
chinaaap <aaa.bbfive@gmail.com>
2024-09-18 16:26:41 +0900

    イベント計算

--- config/app.php
+++ config/app.php
@@ -184,7 +184,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // 'Example' => App\Facades\Example::class,
+        'Constant' => App\Constants\EventConst::class
     ])->toArray(),
 
 ];
